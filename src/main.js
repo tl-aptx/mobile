4aa2252530fc670dac19a37ed7dbe63a15b42571
tanglin <tl4869@126.com>
2021-05-18 00:07:36 +0800

    two

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 全局样式
 import './styles/index.less'
+// vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
